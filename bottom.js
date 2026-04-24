e8e25bf261dcb667591fbadc63ec1e99628925e8
b2029vclco <b2029vclco@pshs.edu.ph>
2026-04-24 21:53:43 +0800

    finished project

--- bottom.js
+++ bottom.js
@@ -1,10 +1,21 @@
-    const KEY_BREATHING_STYLE = "KEY_BREATHING_STYLE";  
+    const KEY_HISTORY = "KEY_HISTORY";  
+
+    //latest breathing style is the last entry in the history
+    function getLatestBreathingStyle() {
+      let history = JSON.parse(localStorage.getItem(KEY_HISTORY)) || [];
+      if (history.length > 0) {
+        return history[history.length - 1].result;
+      }
+      return null;
+    }
 
     //show user's breathing style in nav bar
     function updateNavBreathingStyle() {
-      let breathingStyle = localStorage.getItem(KEY_BREATHING_STYLE);
+      let breathingStyle = getLatestBreathingStyle();
       if (breathingStyle) {
         document.getElementById("navBreathingStyle").innerHTML = "&nbsp;("+breathingStyle+")";
+      } else {
+        document.getElementById("navBreathingStyle").innerHTML = "";
       }
     }
     updateNavBreathingStyle();
